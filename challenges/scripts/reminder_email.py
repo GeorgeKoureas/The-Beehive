250e27bd4f710fed1d64f04ceb3c83b74054d1c7
George <george.s.koureas@gmail.com>
2020-05-27 10:38:25 +0300

    improving mailing notifications

--- challenges/scripts/reminder_email.py
+++ challenges/scripts/reminder_email.py
@@ -1,12 +1,14 @@
+import os
 from datetime import date
 from datutil.relativedelta import relativedelta
 from django.mail import send_mail
-from challenges.models import Challenge
+from .models import Challenge
 
 
 def run():
     for challenge in Challenge.objects.filter(apply_until_date=date.today() + relativedelta(days=5)):
         for applicant in challenge.applicants.all():
+            print(applicant.email)
             send_mail(
                 'Subject here',
                 'Applications are closing',
@@ -14,3 +16,4 @@ def run():
                 [applicant.email],
                 fail_silently=False,
                 )
+            os.sytem("pause")
