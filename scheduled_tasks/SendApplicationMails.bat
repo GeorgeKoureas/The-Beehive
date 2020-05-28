8bb33a5cc76dc4c136746c9fbba36df307441841
George <george.s.koureas@gmail.com>
2020-05-28 06:58:57 +0300

    Deleted the bat file for the scheduled job. The job is now handlend with windows schtasks. When deployed on Linux we will move to cron

--- scheduled_tasks/SendApplicationMails.bat
+++
@@ -1,6 +0,0 @@
-SET log_file=%cd%\logfile.txt
-call C:\Users\Patran\Anaconda3\Scripts\activate.bat
-conda activate Beehive
-cd C:\Users\Patran\Desktop\Projects\Beehive\Beehive
-python manage.py runscript reminder_email
-pause
